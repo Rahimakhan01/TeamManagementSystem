3ec129061dd9543bc66333fec571da1a9f088d7c
Rahimakhan01 <khanrahima1311@gmail.com>
2025-05-02 02:30:33 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'https://team-management-backend-ui67.onrender.com'
+      '/api': 'http://localhost:5001'
     }
   }
 });
