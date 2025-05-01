ae793d1e4c67da33e655103cddb826199af51f71
Rahimakhan01 <khanrahima1311@gmail.com>
2025-05-01 23:52:29 +0530

    backend setup

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'https://team-management-system-henna.vercel.app'
+      '/api': 'http://localhost:5001'
     }
   }
 });
